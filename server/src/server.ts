60f6c7049e5bb52e0fa48911fbce68114ecce9c8
caducasara <carloscasara98@gmail.com>
2022-11-02 14:17:01 -0300

    implementing api routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
     //retorna o log de todas as querys que são realizadas no banco de dados
@@ -17,10 +19,41 @@ async function bootstrap() {
         origin: true
     })
 
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count }
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count }
+    });
+
     fastify.get('/pools/count', async () => {
         const count = await prisma.pool.count();
 
         return { count }
+    });
+
+    fastify.post('/pools', async (request, response) => {
+        const createPoolBody = z.object({
+            title: z.string()
+        })
+
+        const { title } = createPoolBody.parse(request.body);
+
+        const generetedPoolCode = new ShortUniqueId({ length: 6 })
+        const code = String(generetedPoolCode()).toUpperCase();
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        });
+
+        return response.status(201).send({ code });
     });
 
     await fastify.listen({ port:3333, /*host: '0.0.0.0'*/ })
